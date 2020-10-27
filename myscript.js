fd662b0dbd4c4b8b5b7c05fc9631f20d2ec859fb
fidelostorga <71280603+fidelostorga@users.noreply.github.com>
2020-10-27 16:50:51 -0600

    Create myscript.js

---
+++ myscript.js
@@ -0,0 +1,24 @@
+<script language="JavaScript">
+<!--
+function calculateBmi() {
+var weight = document.bmiForm.weight.value
+var height = document.bmiForm.height.value
+if(weight > 0 && height > 0){	
+var finalBmi = (703 * weight)/(height * height)
+document.bmiForm.bmi.value = finalBmi
+if(finalBmi < 18.5){
+document.bmiForm.meaning.value = "you need to eat more."
+}
+if(finalBmi > 18.5 && finalBmi < 25){
+document.bmiForm.meaning.value = "keep up the good work you are healthy."
+}
+if(finalBmi > 25){
+document.bmiForm.meaning.value = "please start exercising."
+}
+}
+else{
+alert("wrong info was inserted")
+}
+}
+//-->
+</script>
